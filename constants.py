6121d45298e5eda5bfcd25302ef4a728b077236d
Lilian Weng <lilian.wengweng@gmail.com>
2012-08-02 23:35:14 -0400

    change constants

--- constants.py
+++ constants.py
@@ -3,13 +3,12 @@
 @author: Lilian Weng (lilian.wengweng@gmail.com)
 '''
 URL_NM = 'http://www.neimanmarcus.com'
-URL_NM_SALE_BAG = \
-    'http://www.neimanmarcus.com/etemplate/et1.jsp?N=4294967029'
+URL_NM_SALE_BAG = 'http://www.neimanmarcus.com/etemplate/et1.jsp?N=4294967029'
 
 
 SMTP_HOST = "localhost"
-FROM_EMAIL = "lilian.wengweng@gmail.com"
-TO_EMAILS = ["lilian.wengweng@gmail.com", "coocafez@gmail.com"]
+FROM_EMAIL = "youremail@youremail.com"
+TO_EMAILS = ["youremail@youremail.com"]
 
 # sec
-DURATION = 60*15
+DURATION = 60*30
